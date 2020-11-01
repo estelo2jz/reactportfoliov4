edeeba18c9950b162d0317dd893c16c1ba2ca4e3
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-11-01 06:55:27 -0600

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ export default class About extends Component {
           <h1>Hello, I'm Estelo!</h1>
           <div className="main__content">
             <div className="main__text">
-              <h2>I am a Front-End Web Developer currently living in Beach Park, Illinois.</h2>
+              <h2>I'm a Front-End Web Developer currently living in Beach Park, Illinois.</h2>
               <p>
                 I like to code starting from scratch, and see where it leads me. 
                 I like to animate things and bringing ideas to life in the browser. I value simple content structure, clean design patterns, and thoughtful user experience.
